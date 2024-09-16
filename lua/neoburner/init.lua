5f4875c1b985eb867c176c73b82c78562c7e856c
John-Bush14 <joppe@stuyck.be>
2024-09-16 18:18:02 +0200

    feat: store config + connection on M

--- lua/neoburner/init.lua
+++ lua/neoburner/init.lua
@@ -8,18 +8,17 @@ local api = require("neoburner.api")
 function M.setup(config)
    inputChecker.config(config)
 
+   M.config = config
 
    local auth_token = config.auth_token
    local port = config.port
    local address = config.address
 
-
-   local connection = api.connect(auth_token, port, address)
+   M.connection = api.connect(auth_token, port, address)
 end
 
 
 function M.show_ram()
-
 end
 
 
