b3d97f10031be115e73a678fbfbc516d74db2bba
John-Bush14 <joppe@stuyck.be>
2025-07-13 18:22:32 +0200

    refact(init): update to accomedate server.lua changes

--- lua/neoburner/init.lua
+++ lua/neoburner/init.lua
@@ -10,12 +10,12 @@ function M.setup(config)
    M.config = config
 
    M.server = new_server(config)
-   M.server:start_listening()
+   M.server:start_listening(function(msg) print(msg) end)
 end
 
 
 function M.show_ram()
-   M.server:connect()
+   M.server:send_message("getFile", {filename = "", server = "home"})
 end
 
 
