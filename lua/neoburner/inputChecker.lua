d1e43ba991af84af2cbff5d8864c72aa58d71197
John-Bush14 <joppe@stuyck.be>
2025-07-16 18:20:33 +0200

    refactor(inputChecker): make type var's their respective type so correct_type can always be type(item)

--- lua/neoburner/inputChecker.lua
+++ lua/neoburner/inputChecker.lua
@@ -14,25 +14,19 @@ end
 
 local function assert_config(config, optionality, keys)
    for key, item in pairs(keys) do
-      local correct_type = ""
-
       if optionality == "required" then
          assert(config[key], "'" .. key .. "' missing from neoburner config (required)")
-
-         correct_type = item
       else
          if config[key] == nil then config[key] = item end
-
-         correct_type = type(item)
       end
 
-      assert_type(config, key, correct_type)
+      assert_type(config, key, type(item))
    end
 end
 
 
 local str = "string"
-local int = "number"
+local int = 0
 
 
 function M.config(config)
