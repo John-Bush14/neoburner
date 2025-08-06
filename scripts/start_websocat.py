1690c35989475d69f8cb334d954d42930177f442
John-Bush14 <joppe@stuyck.be>
2025-08-07 01:04:48 +0200

    fix(start_webocat.py): only god knows

--- scripts/start_websocat.py
+++ scripts/start_websocat.py
@@ -1,9 +1,12 @@
 from subprocess import run
 from signal import pause
 from sys import argv
+from os import open as os_open, O_RDWR, O_NONBLOCK, fdopen
+
+in_fd = os_open(argv[1], O_RDWR, O_NONBLOCK)
 
 with open(argv[1], "r") as infile, open(argv[2], "w") as outfile:
     run(["websocat", "-s", argv[3], "-E"], stdin=infile, stdout=outfile)
 
-with open(argv[1], "w") as infile, open(argv[2], "r") as outfile:
+with fdopen(in_fd, "w") as infile, open(argv[2], "r") as outfile:
     pause()
