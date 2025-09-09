b4d4b2c995f047e22d6dbce23fb53d0580d1ab27
John-Bush14 <joppe@stuyck.be>
2025-09-09 11:59:30 +0200

    feat(filesystem): function to refresh single server

--- lua/neoburner/filesystem.lua
+++ lua/neoburner/filesystem.lua
@@ -16,4 +16,18 @@ function FS.reinititialize()
    os.execute('mkdir -p "' .. FS.root .. FS.servers_folder .. '"')
 end
 
+function FS.refresh(server, SERVER)
+   local root = FS.root .. (FS.servers_folder and server ~= FS.root_server or "")
+
+   local files =  SERVER:use_remote_method("get_all_files", {server = server})
+
+   for file, content in pairs(files) do
+      local fh = io.open(root .. file, "w+")
+
+      assert(fh ~= nil, error("Problem opening up file '" .. file .. "' for server '" .. server .. "'"))
+
+      fh.write(content)
+   end
+end
+
 return new
