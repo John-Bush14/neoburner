0dbbb5eb9baf49e341295ef8cea6b3d666bcbf9e
John-Bush14 <joppe@stuyck.be>
2025-09-09 11:51:26 +0200

    feat(filesystem): reinitialize function which rm's and mkdir dir's

--- lua/neoburner/filesystem.lua
+++ lua/neoburner/filesystem.lua
@@ -5,7 +5,15 @@ function new(config)
    FS.root_server = config.root_server
    FS.servers_folder = config.servers_folder
 
+   if string.sub(FS.root, string.len(FS.root)) ~= "/" then FS.root = FS.root .. "/" end
+
    return FS
+end
+
+function FS.reinititialize()
+   vim.fs.rm(FS.root)
+
+   os.execute('mkdir -p "' .. FS.root .. FS.servers_folder .. '"')
 end
 
 return new
