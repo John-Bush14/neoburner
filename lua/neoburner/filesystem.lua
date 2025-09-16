c785aba709b1e148019b2badf51aedae57c9eeda
John-Bush14 <joppe@stuyck.be>
2025-09-16 11:58:38 +0200

    feat(filesystem): normalize filesystem path

--- lua/neoburner/filesystem.lua
+++ lua/neoburner/filesystem.lua
@@ -1,7 +1,7 @@
 local FS = {}
 
 local function new(config)
-   FS.root = config.filesystem
+   FS.root = vim.fs.normalize(config.filesystem)
    FS.root_server = config.root_server
    FS.servers_folder = config.servers_folder
 
