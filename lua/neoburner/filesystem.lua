49d63c9422080799292f14445f737e9d129c22ef
John-Bush14 <joppe@stuyck.be>
2025-09-18 13:45:16 +0200

    fix(filesystem): clear all autocmds of a server before creating them

--- lua/neoburner/filesystem.lua
+++ lua/neoburner/filesystem.lua
@@ -37,10 +37,13 @@ end
 
 local autocmds = {}
 function FS:set_up_autocmds(server, SERVER)
+   local pattern = vim.fs.joinpath(FS:get_server_root(server), "*")
+
+   vim.api.nvim_clear_autocmds({pattern})
+
    for event, remote_method in pairs(autocmds) do
       vim.api.nvim_create_autocmd(event, {
-         pattern = vim.fs.joinpath(FS:get_server_root(server), "*"),
-         callback = function(ev)
+         pattern, callback = function(ev)
             SERVER:use_remote_method(remote_method, {file = ev.match, server})
          end
       })
